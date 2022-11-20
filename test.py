429723a8ab7adeed88ee23c0f132cbf8ea699bc5
denpayanara <88641815+denpayanara@users.noreply.github.com>
2022-11-20 11:50:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-import requests
+import json
+import urllib.request
+import ssl
 
-r = requests.get("https://www.tele.soumu.go.jp/musen/list?ST=1&DA=1&SC=1&DC=1&OF=2&OW=FB_H&IT=E&NA=%8Ay%93V%83%82%83o%83C%83%8B")
+url = 'https://www.tele.soumu.go.jp/musen/list?ST=1&DA=1&SC=1&DC=1&OF=2&OW=FB_H&IT=E&NA=%8Ay%93V%83%82%83o%83C%83%8B'
 
-print(r.json())
+ctx = ssl.create_default_context()
+ctx.options |= 0x4  # ssl.OP_LEGACY_SERVER_CONNECT
+
+# req = urllib.request.urlopen(url, context=ctx)
+
+try:
+    with urllib.request.urlopen(url, context=ctx) as res:
+        print(res.read())
+except urllib.error.HTTPError as err:
+    print(err.code)
+except urllib.error.URLError as err:
+    print(err.reason)
