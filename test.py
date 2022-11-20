0bdcdc40ad9ef2b224f36bdd775528e81fe65203
denpayanara <88641815+denpayanara@users.noreply.github.com>
2022-11-20 11:25:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
-r = requests.get("https://www.tele.soumu.go.jp/musen/list/ST=1&DA=1&SC=1&DC=1&OF=2&OW=FB_H&IT=E&NA=%8Ay%93V%83%82%83o%83C%83%8B")
+r = requests.get("https://www.tele.soumu.go.jp/musen/list?ST=1&DA=1&SC=1&DC=1&OF=2&OW=FB_H&IT=E&NA=%8Ay%93V%83%82%83o%83C%83%8B")
 
 print(r.json())
